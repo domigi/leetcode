27baa0f41f9bfb4dbc6c6fdcff5625bd99c09404
Dominic Gisin <gisindom@students.zhaw.ch>
2022-04-21 20:22:45 +0200

    done

--- 002-addTwoNumbers.go
+++ 002-addTwoNumbers.go
@@ -7,16 +7,29 @@
  */
  func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
     carry := 0
-    var result ListNode{
-        Val: l1.Val + l2.Val
-    }
-    for l1.Next != nil || l2.Next != nil {
-        sum := l1.Val + l2.Val + carry
+     result := ListNode{Val:0}
+    var current *ListNode = &result
+    for l1 != nil || l2 != nil || carry == 1  {
+        sum := carry
         carry = 0
-        if sum > 10 {
-            carry = 1
+        if l1 != nil{
+            sum += l1.Val
+            l1 = l1.Next
         }
+        if l2 != nil {
+            sum += l2.Val
+            l2 = l2.Next
+        }
+        if sum > 9 {
+            carry = 1
+            sum %= 10
+        }
+        current.Val = sum
         
+        if l1 != nil || l2 != nil || carry == 1{
+        current.Next = &ListNode{}
+        current = current.Next
+        }
     }
-    return nil
+    return &result
 }
